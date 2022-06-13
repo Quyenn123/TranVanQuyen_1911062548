d528774e45ee800471aa967b1d109c41b648c984
Quyenn123 <91267155+Quyenn123@users.noreply.github.com>
2022-06-13 15:23:13 +0700

    Add project files.

---
+++ TranVanQuyen_1911062548/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace TranVanQuyen_1911062548
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ TranVanQuyen_1911062548/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TranVanQuyen_1911062548.MvcApplication" Language="C#" %>
